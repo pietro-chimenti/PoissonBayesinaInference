942b93f322520e477b23f07578d2da1b82224176
orionvillanueva25 <orionvillanuevafilho@gmail.com>
2023-04-04 23:13:41 -0300

    Reformulação do Código sem Cadeia de Markov por Orion

--- Models/BasicPoisson.py
+++ Models/BasicPoisson.py
@@ -1,97 +1,91 @@
 #!/usr/bin/env python
 """This files provides basic tools to make bayesian inferences on simple poissonian models.
 """
-import warnings
+
+import sys
 import numpy as np
 from scipy.stats import poisson
-from scipy.stats import gamma
-import matplotlib.pyplot as plt
-import math
-import emcee
-
+from scipy.stats import gamma 
+import pandas as pd
 
 
 class BasicPoisson :
     """This class represent a simple poissonian model for bayesian analysis"""
     
-    ndim, nwalkers = 1, 100 
-    big_integer = 100
-
-
-    def log_like(self, mu, data):
-        """ This function represent the logarithm of the poissonian likelihood"""
-        if mu > 0 :
-            if data < self.big_integer : 
-                return data*np.log(mu)-mu-np.log(float(math.factorial(data)))
-            else : # use stirling! Need to improve terms.
-                return data*np.log(mu)-mu-data*np.log(data)+data
-        else: return -1*math.inf
-
-    def log_prior_uniform(self, mu):
-        if mu <= 0: return -1*math.inf
-        return 0
-
-    def log_prior_jeffreys(self, mu):
-        if mu <= 0: return -1.*math.inf
-        return -0.5*mu
+    ''' Update Parameters '''
+    def gamma_change(self,data, shape, scale):
+        r = np.sum(data) + shape
+        v = len(data) + scale
+        return r, v
     
-    def log_prior_gamma(self, mu):
-        if mu <= 0: return -1.*math.inf
-        return (self.r-1)*mu-self.v*mu
-    
-    def log_prob(self, mu):
-        lp = self.log_prior(mu)
-        if lp == -1.*math.inf: return -1.*math.inf, 0, 0, 0, 0
-        ll = self.log_like(mu, self.ov)
-        if ll == -1.*math.inf: return -1.*math.inf, 0, 0, 0, 0
-        posterior_sample = poisson.rvs(mu)
-        psl = self.log_like(mu, posterior_sample)
-        return ll+lp, ll, lp, posterior_sample, psl
-    
-    def __init__(self, observed_value, prior = 'uniform', mean = 0, std = 0, *args):
+    '''Record the variables'''
+    def __init__(self, observed_value, events_number, prior = 'uniform', mean = 0, std = 0, *args):
+        
+        # Sampler
         self.ov = observed_value
-
+        self.ev = events_number
+        
+        self.data = poisson.rvs(mu = self.ov, size = self.ev)
+        
+        # New Parameters
         if prior == 'uniform':
-            self.log_prior = self.log_prior_uniform
+            self.r , self.v = self.gamma_change(data = self.data, shape= 0.5, scale = 0)
         if prior == 'jeffreys':
-            self.log_prior = self.log_prior_jeffreys 
+            self.r , self.v = self.gamma_change(data = self.data, shape= 1, scale = 0)
         if prior == 'gamma':
             if mean == 0 or std == 0: raise ValueError('Prior mean and std must be positive!')
-            self.r = mean**2/std**2
-            self.v = mean/std**2
-            self.log_prior = self.log_prior_gamma
-
-        self.mu_min = round(self.ov-5*math.sqrt(self.ov))
-        self.mu_max = round(self.ov+5*math.sqrt(self.ov))
-        if self.mu_min < 0: self.mu_min = 0
-        if self.ov == 0 :
-            self.mu_min = 0
-            self.mu_max = 5
+            self.shape = mean**2/std**2
+            self.scale = mean/std**2
+            self.r , self.v = self.gamma_change(data = self.data, shape= self.shape, scale = self.scale)
         
-        self.samples_list = []
-        self.blobs_list   = []
-        self.acceptance_fraction_list = []
-        self.autocorr_time_list = []
+        # Posterior Distribution
+        self.interval = np.linspace(gamma.ppf(0.01,self.r, scale=1/self.v), gamma.ppf(0.999, self.r,scale=1/self.v),100)
+        self.distribution = gamma.pdf(self.interval, a = self.r, scale = 1/self.v)
         
-    def run(self, samples = 1000, seed = 42):
-        if self.samples_list:
-            warnings.warn("Sampler already run!")
+        # Data Summary
+        self.mean, self.var = gamma.stats(self.r, scale = 1/self.v, moments='mv')
+        self.median = gamma.median(self.r, scale = 1/self.v)
+        self.mode = (self.r-1)/self.v
+        self.up = gamma.ppf(0.75, self.r, scale = 1/self.v)
+        self.low = gamma.ppf(0.25, self.r, scale = 1/self.v)
+        self.IQR = self.up - self.low
+        
+        #Table
+        self.summary = pd.Series({'Mean': self.mean,'Median': self.median,'Mode': self.mode,'Variance': self.var, 'IQR' : self.IQR})
+        self.df = pd.DataFrame([self.summary])
+    
+    '''Print the Data Table'''
+    def data_summarry(self):
+        print(self.df)
+    
+    '''Take the probability of an interval'''
+    def probability_calculation(self):
+        print('Choose the type of probability')
+        print(' Lower Probability - 1\n Upper Probability - 2 \n Interval Probability - 3')
+        print('Insert the number of the option:')
+        option = input()
+        if option == '1':
+            print('Insert mu value:')
+            value = input()
+            x = float(value)
+            prob = gamma.cdf(x, self.r, scale = 1/self.v)
+            print(f'The probability of mu < {x} is {prob}')
+        elif option == '2':
+            print('Insert mu value:')
+            value = input()
+            x = float(value)
+            prob = 1 - gamma.cdf(x, self.r, scale = 1/self.v)
+            print(f'The probability of mu > {x} is {prob}')
+        elif option == '3':
+            print('Insert lower value:')
+            lower = input()
+            print('Insert upper value:')
+            upper = input()
+            low = float(lower)
+            up = float(upper)
+            prob = gamma.cdf(up, self.r, scale = 1/self.v) - gamma.cdf(low, self.r, scale = 1/self.v)
+            print(f'The probability of {low} < mu < {up} is {prob}')
         else:
-            np.random.seed(seed)
-            for i in range(8):
-                print("Running chain n.",i)
-                p0 = self.mu_max*np.absolute(np.random.randn(self.nwalkers, self.ndim))
-                sampler = emcee.EnsembleSampler(self.nwalkers, self.ndim, self.log_prob)
-                sampler.run_mcmc(p0, samples)
-                self.samples_list.append(sampler.get_chain(flat=True))
-                self.blobs_list.append(sampler.get_blobs())
-                self.acceptance_fraction_list.append(sampler.acceptance_fraction)
-                self.autocorr_time_list.append(sampler.get_autocorr_time())
-            self.diagnose()
-            
-    def diagnose(self):
-        for i in range(8):
-            print("Chain ",i)
-            print( "Mean acceptance fraction: {0:.3f}".format(np.mean(self.acceptance_fraction_list[i])))
-            print( "Mean autocorrelation time: {0:.3f} steps".format(np.mean(self.autocorr_time_list[i])))
+            print("Invalid Input")
+            sys.exit(0)
             
