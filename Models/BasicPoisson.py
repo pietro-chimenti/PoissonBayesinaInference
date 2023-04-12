6d18f80a9ec38e6c84013b01d0aade91a866c34c
orionvillanueva25 <orionvillanuevafilho@gmail.com>
2023-04-11 23:15:06 -0300

    Documentação + Intervalo de Credibilidade
    Ajuste no input do valor observado. Melhoria da separação entre modelo e teste. Começo de organização com a documentação (dosctring). Introdução de 3 tipos de intervalos de credibilidade.

--- Models/BasicPoisson.py
+++ Models/BasicPoisson.py
@@ -4,6 +4,7 @@
 
 import sys
 import numpy as np
+from scipy import optimize
 from scipy.stats import poisson
 from scipy.stats import gamma 
 import pandas as pd
@@ -11,35 +12,45 @@ import pandas as pd
 
 class BasicPoisson :
     """This class represent a simple poissonian model for bayesian analysis"""
-    
-    ''' Update Parameters '''
+
     def gamma_change(self,data, shape, scale):
-        r = np.sum(data) + shape
-        v = len(data) + scale
+        '''
+        This function caculates the updated parameters analytically of a gamma
+        posterior distribution Gamma(r,v).
+            Parameters:
+                data (int): observed value, used as likelyhood
+                shape (int): first prior parameter
+                scale (int): second prior parameter
+            Returns:
+                r (int): updater shape parameter
+                v (int): updated scale parameter
+        '''
+        r = data + shape
+        v = 1 + scale
         return r, v
     
-    '''Record the variables'''
-    def __init__(self, observed_value, events_number, prior = 'uniform', mean = 0, std = 0, *args):
-        
-        # Sampler
+    def __init__(self, observed_value, prior = 'uniform', mean = 0, std = 0, *args):
+        '''
+        Receive the users data and the prior used (Uniform, Jeffrey´s, Gamma), 
+        and calculate the posterior distribution.
+        '''
+    
+        # Users Data
         self.ov = observed_value
-        self.ev = events_number
-        
-        self.data = poisson.rvs(mu = self.ov, size = self.ev)
         
         # New Parameters
         if prior == 'uniform':
-            self.r , self.v = self.gamma_change(data = self.data, shape= 0.5, scale = 0)
+            self.r , self.v = self.gamma_change(data = self.ov, shape= 0.5, scale = 0)
         if prior == 'jeffreys':
-            self.r , self.v = self.gamma_change(data = self.data, shape= 1, scale = 0)
+            self.r , self.v = self.gamma_change(data = self.ov, shape= 1, scale = 0)
         if prior == 'gamma':
             if mean == 0 or std == 0: raise ValueError('Prior mean and std must be positive!')
             self.shape = mean**2/std**2
             self.scale = mean/std**2
-            self.r , self.v = self.gamma_change(data = self.data, shape= self.shape, scale = self.scale)
+            self.r , self.v = self.gamma_change(data = self.ov, shape= self.shape, scale = self.scale)
         
         # Posterior Distribution
-        self.interval = np.linspace(gamma.ppf(0.01,self.r, scale=1/self.v), gamma.ppf(0.999, self.r,scale=1/self.v),100)
+        self.interval = np.linspace(gamma.ppf(0.0001,self.r, scale=1/self.v), gamma.ppf(0.9999, self.r,scale=1/self.v),100)
         self.distribution = gamma.pdf(self.interval, a = self.r, scale = 1/self.v)
         
         # Data Summary
@@ -50,42 +61,58 @@ class BasicPoisson :
         self.low = gamma.ppf(0.25, self.r, scale = 1/self.v)
         self.IQR = self.up - self.low
         
-        #Table
-        self.summary = pd.Series({'Mean': self.mean,'Median': self.median,'Mode': self.mode,'Variance': self.var, 'IQR' : self.IQR})
+        # Summary Table
+        self.summary = pd.Series({'Mean': self.mean,'Median': self.median,'Mode': self.mode,'Variance': self.var,'Low Quartile':self.low,'Up Quartile': self.up ,'IQR' : self.IQR})
         self.df = pd.DataFrame([self.summary])
     
-    '''Print the Data Table'''
     def data_summarry(self):
-        print(self.df)
-    
-    '''Take the probability of an interval'''
-    def probability_calculation(self):
-        print('Choose the type of probability')
-        print(' Lower Probability - 1\n Upper Probability - 2 \n Interval Probability - 3')
-        print('Insert the number of the option:')
-        option = input()
-        if option == '1':
-            print('Insert mu value:')
-            value = input()
-            x = float(value)
-            prob = gamma.cdf(x, self.r, scale = 1/self.v)
-            print(f'The probability of mu < {x} is {prob}')
-        elif option == '2':
-            print('Insert mu value:')
-            value = input()
-            x = float(value)
-            prob = 1 - gamma.cdf(x, self.r, scale = 1/self.v)
-            print(f'The probability of mu > {x} is {prob}')
-        elif option == '3':
-            print('Insert lower value:')
-            lower = input()
-            print('Insert upper value:')
-            upper = input()
-            low = float(lower)
-            up = float(upper)
-            prob = gamma.cdf(up, self.r, scale = 1/self.v) - gamma.cdf(low, self.r, scale = 1/self.v)
-            print(f'The probability of {low} < mu < {up} is {prob}')
+        """ Prints the posterior distribution summaries"""
+        
+        print("Posterior Summaries")
+        print(self.df.T)
+        
+    def credible_interval(self,trust = 0.95, option = 1):
+        '''
+        Calculate the Credible Interval of the Posterior Gamma Distribution.
+            Parameters:
+                trust(float): confidence interval choosen by the user
+                option(int): 1- Upper Limit Interval; 2- Symmetrical Interval; 
+                3- Highest Density Interval
+            Returns:
+                lower_limit(float): lower point of the interval
+                upper_limit(float): highest point of the interval
+        '''
+        if option == 1:
+            lower_limit = gamma.ppf(0.0001, self.r, scale = 1/self.v)
+            upper_limit = gamma.ppf(trust, self.r, scale = 1/self.v)
+            print(f'The upper limit credible interval is: ({lower_limit},{upper_limit})')
+            return lower_limit, upper_limit
+        elif option == 2:
+            itv = (1 - trust)/2
+            lower_limit = gamma.ppf(itv, self.r, scale = 1/self.v)
+            upper_limit = gamma.ppf(1 - itv , self.r, scale = 1/self.v)
+            print(f'The Symmetrical credible interval is: ({lower_limit},{upper_limit})')
+            return lower_limit, upper_limit
+        elif option == 3:
+            
+            def interval_width(lower):
+                '''
+                For lower limit, calculate the variation between the upper and lower
+                separeted by the credible interval.
+                '''
+                upper = gamma.ppf(gamma.cdf(lower, self.r, scale = 1/self.v) + trust, self.r, scale = 1/self.v)
+                return upper - lower
+            #média simetrica como chute
+            initial_guess = gamma.ppf((1-trust)/2, self.r, scale = 1/self.v)
+            #acha os valores que minimiza a função 
+            optimize_result = optimize.minimize(interval_width, initial_guess)
+            #retira o valor correspondente
+            lower_limit = optimize_result.x[0]
+            width = optimize_result.fun
+            #acha o valor superior somando ao intervalo
+            upper_limit = lower_limit + width
+            print(f'The High Density Credible Interval is: ({lower_limit},{upper_limit})')
+            return lower_limit, upper_limit
         else:
             print("Invalid Input")
             sys.exit(0)
-            
--- Tests/TestBasicPoisson.py
+++ Tests/TestBasicPoisson.py
@@ -9,6 +9,8 @@ from Models import BasicPoisson as BP
 
 def main():  
     
+#está parte está comentada para retirar o input do usario, facilitando testes
+    '''
     #inserindo constantes
     print("Input the observed value:")
     ov_string = input()
@@ -20,36 +22,55 @@ def main():
     if ov < 0 :
         print("Observed value must not be negative!")
         sys.exit(0)
-        
-    print("Input number of events:")
-    ev_string = input()
-    try:
-        ev = int(ev_string)
-    except ValueError:
-        print("Value Error: must be an integer!")
-        sys.exit(0)    
-    if ev < 0 :
-        print("Observed value must not be negative!")
-        sys.exit(0)
 
+    print('Choose the type of credible interal')
+    print(' Upper Limit Interval - 1\n Symmetrical Interval - 2 \n High Density Interval - 3')
+    print('Insert the number of the option:')
+    option = input()
+    print('Insert the Credible Interval %:')
+    inp = input()
+    trust = int(inp)/100
+    '''    
+    
+    #constantes
+    ov = 5
+    credible_interval = 0.95
+    prior = 'gamma'
+    mean = 4
+    stan_desv = 2
+    
     #rodadando o código
-    model = BP.BasicPoisson(observed_value=ov, events_number = ev ,prior='jeffreys')
+    model = BP.BasicPoisson(observed_value=ov,prior = prior, mean = mean, std = stan_desv)
     x = model.interval
     y = model.distribution
+    
+    #Intervalo de Credibilidade Limite Superior
+    up1, down1 = model.credible_interval(trust = credible_interval, option = 1)
+
+    #Intervalo de Credibilidade Simetrico
+    up2, down2 = model.credible_interval(trust= credible_interval, option = 2)
+    
+    #Intervalo de Credibilidade HDI
+    up3, down3 = model.credible_interval(trust= credible_interval, option = 3)
     
     #gráfico
     plt.plot(x, y)
     plt.title("Posterior Distribution")
     plt.xlabel("mu")
     plt.ylabel("p.d.f.")
+    plt.axvline(x = up1, color = 'r', label = 'Upper limit')
+    plt.axvline(x = down1, color = 'r')
+    plt.axvline(x = up2, color = 'g', label = 'Symmetrical')
+    plt.axvline(x = down2, color = 'g')
+    plt.axvline(x = up3, color = 'm', label = 'HDI')
+    plt.axvline(x = down3, color = 'm')
+    plt.legend()
     plt.grid()
     plt.show()  
     
     #tabela de dados
     model.data_summarry()
-    
-    #calculo de probabilidade
-    model.probability_calculation()
+
 
 if __name__ == "__main__":
     main()
