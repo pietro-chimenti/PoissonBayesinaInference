d724887aa4020e7abb38db8ccb4d7eceb20e0b69
orionvillanueva25 <orionvillanuevafilho@gmail.com>
2023-08-27 20:06:42 -0300

    Add p0 sampler
    The p0 array for the emcee MCMC chain will be drawn from a gamma distribution, utilizing parameters derived from the mean and standard deviation of the data.

--- Models/SignalAndNoise.py
+++ Models/SignalAndNoise.py
@@ -6,6 +6,7 @@
 import math 
 import numpy as np
 from scipy.stats import gamma 
+import emcee 
 
 class SignalAndNoise :
     """This class represent signal(on) and noise(off) parameter poissonian inference model for 
@@ -108,18 +109,34 @@ class SignalAndNoise :
         self.beta = mean_on/std_on**2
         self.alpha = mean_off**2/std_off**2
         self.beta = mean_off/std_off**2
-        
-        print('it´s working!')
                 
-    def log_posterior(self, mu):
-        self.lp_on = self.log_prior_on(mu = mu,alpha = self.alpha, beta = self.beta)
-        self.lp_off = self.log_prior_off(mu = mu,alpha = self.alpha, beta = self.beta)
-        self.ll_on = self.log_like_off(mu = mu,data = self.ov_off)
-        self.ll_off = self.log_like_on(mu = mu,data = self.ov_on)
-            
-        return float(self.lp_on[1]) + float(self.lp_off[0]) + self.ll_on + self.ll_off
-            
+        def log_posterior(self, mu):
+            self.lp_on = self.log_prior_on(mu = mu,alpha = self.alpha, beta = self.beta)
+            self.lp_off = self.log_prior_off(mu = mu,alpha = self.alpha, beta = self.beta)
+            self.ll_on = self.log_like_off(mu = mu,data = self.ov_off)
+            self.ll_off = self.log_like_on(mu = mu,data = self.ov_on)
+                
+            return float(self.lp_on[1]) + float(self.lp_off[0]) + self.ll_on + self.ll_off
         
+        self.ndim, self.nwalkers = 2, 100
+        
+        # stats of a gamma sampler for p0
+        m_off = np.mean(self.ov_off)
+        dp_off = np.std(self.ov_off)
+        m_on = np.mean(self.ov_on)
+        dp_on = np.std(self.ov_on)
+
+        self.p0 = np.array([np.random.gamma([m_off**2/dp_off**2,m_on**2/dp_on**2 ], #first value
+                                                    scale=[dp_off**2/m_off,dp_on**2/m_on])])
+                            
+        for i in np.arange(self.nwalkers-1):
+            gamma = np.random.gamma([m_off**2/dp_off**2,m_on**2/dp_on**2 ], 
+                                                        scale=[dp_off**2/m_off,dp_on**2/m_on])
+            self.p0 = np.append(self.p0,[gamma],axis=0)
+        
+        print(self.p0)
+        print('it´s working!')
+
     
         
         
--- Tests/TestSignalAndNoise.py
+++ Tests/TestSignalAndNoise.py
@@ -12,20 +12,7 @@ from Models import SignalAndNoise as SN
 def main():
     
     test1 = SN.SignalAndNoise([1,2], [3,4],'jeffrey','uniform')
-    post1 = test1.log_posterior(mu=[10,2])
-    print(post1)
-    
-    test2 = SN.SignalAndNoise([1,2], [3,4],'gamma','uniform',mean_off= 2, std_off=2)
-    post2 = test2.log_posterior(mu=[10,6])
-    print(post2)
-    
-    test3 = SN.SignalAndNoise([1,2,5,8,10], [3,4,5,3],'gamma','gamma',mean_on=50, std_off=20)
-    post3 = test3.log_posterior(mu=[30,21])
-    print(post3)
-    
-    test4 = SN.SignalAndNoise([-1,2], [3,4],'jeffrey','uniform')
 
-    post4 = test4.log_posterior(mu=[10,-12])
-    print(post4)
+
 if __name__ == "__main__":
     main()
